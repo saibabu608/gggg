06abe10594df0e3ead2be70beb4b1956d3852a67
saibabu608 <147689494+saibabu608@users.noreply.github.com>
2024-01-12 10:47:23 +0530

    Update hhhh.py

--- hhhh.py
+++ hhhh.py
@@ -1,3 +1,4 @@
 import streamlit as st
-st.header('fjdk')
-st.write('rajesh')
+st.header('Great Dane')
+st.write('Great Dane')
+st.image("C:\Users\HP\Downloads\Great-Dane-standing-in-a-fenced-field-400x267.webp")
