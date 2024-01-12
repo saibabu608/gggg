de97da7dafc39bc698be6807a0325c8ad1302191
saibabu608 <147689494+saibabu608@users.noreply.github.com>
2024-01-12 11:19:31 +0530

    Update hhhh.py

--- hhhh.py
+++ hhhh.py
@@ -6,7 +6,7 @@ st.write('Great Dane')
 # Use a valid image URL
 image_url = 'https://images.ctfassets.net/440y9b545yd9/3TzZVoOLDSA79qhNhdyXIK/bc2583abe9daf8591e0aaef69ef616ee/GreatDane850.jpg'
 # Display the image with a caption
-st.image(image_url, caption='Toyota Supra')
+st.image(image_url, caption='Great Dane')
 
 # You can also add alternative text for accessibility
 # st.image(image_url, caption='Great Dane', alt='Toyota Supra Image')
