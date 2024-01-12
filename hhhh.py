7e4c3b1f24abb7a32911490ede0e9a5a65431cf5
saibabu608 <147689494+saibabu608@users.noreply.github.com>
2024-01-12 11:07:51 +0530

    Update hhhh.py

--- hhhh.py
+++ hhhh.py
@@ -1,4 +1,17 @@
 import streamlit as st
-st.header('Great Dane')
-st.write('Great Dane')
-st.image('C:\Users\HP\Downloads\GreatDane850.jpg')
+
+st.header('Supra')
+st.write('Supra')
+
+# Use a valid image URL
+image_url = 'C:\Users\HP\Downloads\GreatDane850.jpg'
+
+# Display the image with a caption
+st.image(image_url, caption='Toyota Supra')
+
+# You can also add alternative text for accessibility
+# st.image(image_url, caption='Toyota Supra', alt='Toyota Supra Image')
+
+
+
+
