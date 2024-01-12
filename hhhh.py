4a608fbc0047bbf537f8c88f2c15b9725e2d66cc
saibabu608 <147689494+saibabu608@users.noreply.github.com>
2024-01-12 11:13:11 +0530

    Update hhhh.py

--- hhhh.py
+++ hhhh.py
@@ -1,10 +1,10 @@
 import streamlit as st
 
-st.header('Supra')
-st.write('Supra')
+st.header('Great Dane')
+st.write('Great Dane')
 
 # Use a valid image URL
-image_url = 'C:\Users\HP\Downloads\GreatDane850.jpg'
+image_url = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.petinsurance.com%2Fhealthzone%2Fpet-breeds%2Fdog-breeds%2Fgreat-dane%2F&psig=AOvVaw1VO1hqGVorhL1DBobLA8k7&ust=1705123182673000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLDd6oGN14MDFQAAAAAdAAAAABAD'
 
 # Display the image with a caption
 st.image(image_url, caption='Toyota Supra')
