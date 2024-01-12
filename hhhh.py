ffa0de0e41b92facc55917bb16daba18a16999cd
saibabu608 <147689494+saibabu608@users.noreply.github.com>
2024-01-12 11:16:02 +0530

    Update hhhh.py

--- hhhh.py
+++ hhhh.py
@@ -4,8 +4,7 @@ st.header('Great Dane')
 st.write('Great Dane')
 
 # Use a valid image URL
-image_url = 'https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.petinsurance.com%2Fhealthzone%2Fpet-breeds%2Fdog-breeds%2Fgreat-dane%2F&psig=AOvVaw1VO1hqGVorhL1DBobLA8k7&ust=1705123182673000&source=images&cd=vfe&opi=89978449&ved=0CBMQjRxqFwoTCLDd6oGN14MDFQAAAAAdAAAAABAD'
-
+image_url = 'https://www.petinsurance.com/healthzone/pet-breeds/dog-breeds/great-dane/'
 # Display the image with a caption
 st.image(image_url, caption='Toyota Supra')
 
