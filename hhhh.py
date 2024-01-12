9057932953a49c0a0b9ae4080d4b909320202b2b
saibabu608 <147689494+saibabu608@users.noreply.github.com>
2024-01-12 10:51:06 +0530

    Update hhhh.py

--- hhhh.py
+++ hhhh.py
@@ -1,4 +1,4 @@
 import streamlit as st
 st.header('Great Dane')
 st.write('Great Dane')
-st.image("C:\Users\HP\Downloads\Great-Dane-standing-in-a-fenced-field-400x267.webp")
+st.image("C:\Users\HP\Downloads\GreatDane850.jpg")
